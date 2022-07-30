5859ec3c94710718a17695dfdd790a802c0ce6ba
Ameen-Here <johnharry125@yahoo.com>
2022-07-30 22:01:36 +0530

    Added a trial route for one review query

--- index.js
+++ index.js
@@ -1,11 +1,75 @@
+import axios from "axios";
 import express from "express";
 const app = express();
 
 const PORT = 3000;
 
+const DATA_SET = [
+  {
+    review: "The product was awesome",
+  },
+  {
+    review: "The product was awesome",
+  },
+  {
+    review: "The product was awesome",
+  },
+  {
+    review: "The product was awesome",
+  },
+  {
+    review: "The product was awesome",
+  },
+  {
+    review: "The product was awesome",
+  },
+  {
+    review: "The product was awesome",
+  },
+  {
+    review: "The product was awesome",
+  },
+  {
+    review: "The product was awesome",
+  },
+  {
+    review: "The product was awesome",
+  },
+  {
+    review: "The product was awesome",
+  },
+  {
+    review: "The product was awesome",
+  },
+  {
+    review: "The product was awesome",
+  },
+  {
+    review: "The product was awesome",
+  },
+  {
+    review: "The product was awesome",
+  },
+  {
+    review: "The product was awesome",
+  },
+];
+
 // Routing
-app.get("/", (req, res) => {
-  res.send("Homepage");
+app.get("/", async (req, res) => {
+  //   const name = await axios.get(
+  //     "https://api.parser.name/?api_key=ur_key&endpoint=generate"
+  //   );
+  //   console.log(name);
+  //   const response = await name.data;
+  //   console.log(response.data[0].name);
+  if (req.query.endpoint) {
+    const randomNum = Math.floor(Math.random() * 20);
+    const data = DATA_SET[randomNum];
+
+    return res.send(data);
+  }
+  res.send("No endpoint");
 });
 
 app.listen(PORT, () => console.log(`Server listening to port ${PORT}`));
