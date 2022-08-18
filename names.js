90ff57ecea0ab123509e760a4d38ddbf988cd9db
Ameen-Here <johnharry125@yahoo.com>
2022-08-18 23:11:01 +0530

    Clean up the code

--- names.js
+++ names.js
@@ -1,11 +1,9 @@
 import axios from "axios";
 
-import germanNames from "./Resource/Names/germanNames.js";
-
 const generateName = async (nameArray) => {
   for (let i = 0; i < 250; i++) {
     const name = await axios.get(
-      "https://api.parser.name/?api_key=14d88c7ba470803d912a260beab6d271&endpoint=generate&country_code=DE"
+      "https://api.parser.name/?api_key=YOUR_API_KEY&endpoint=generate&country_code=COUNTRY_CODE"
     );
     const response = await name.data;
     let newName = `${response.data[0].name.firstname.name} ${response.data[0].name.lastname.name}`;
@@ -15,6 +13,6 @@ const generateName = async (nameArray) => {
   console.dir(nameArray, { maxArrayLength: null });
 };
 
-generateName(germanNames);
+// generateName();
 
 export default generateName;
