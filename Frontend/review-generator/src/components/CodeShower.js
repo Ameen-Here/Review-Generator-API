d41eafbd0f72b8a44daa338915778532fe28c5e7
Ameen-Here <johnharry125@yahoo.com>
2022-08-26 16:27:33 +0530

    Added other tabs and added highlight colors

--- Frontend/review-generator/src/components/CodeShower.js
+++ Frontend/review-generator/src/components/CodeShower.js
@@ -5,19 +5,117 @@ import classes from "./CodeShower.module.css";
 const CodeShower = (e) => {
   const browserCodeRef = useRef();
   const [copyBtnText, setCopyBtnText] = useState("Copy to Clipboard");
-  const [copiedValue, setCopiedValue] = useState("");
+  const [copiedValue, setCopiedValue] = useState(
+    "https://lit-chamber-70662.herokuapp.com/v1/review?apiKey=6e97db3d-269c-4309-ad0a-72ffb83cc0d5"
+  );
+
+  const [btnActive, setBtnActive] = useState({
+    browser: true,
+    curl: false,
+    python: false,
+    node: false,
+    php: false,
+  });
+
+  console.log(btnActive.browser);
+
+  const browserClass = btnActive.browser ? "active" : "";
+  const curlClass = btnActive.curl ? "active" : "";
+  const pythonClass = btnActive.python ? "active" : "";
+  const nodeClass = btnActive.node ? "active" : "";
+  const phpClass = btnActive.php ? "active" : "";
+
   const copyBtnHandler = () => {
-    setCopiedValue(browserCodeRef.current.innerText);
     setCopyBtnText("Copied To Clipboard!!!");
     setTimeout(() => {
       setCopyBtnText("Copy to Clipboard");
     }, 3000);
   };
+
+  const browserBtnHandler = () => {
+    setCopiedValue(
+      `https://lit-chamber-70662.herokuapp.com/v1/review?apiKey=6e97db3d-269c-4309-ad0a-72ffb83cc0d5`
+    );
+    setBtnActive({
+      browser: true,
+      curl: false,
+      python: false,
+      node: false,
+      php: false,
+    });
+  };
+
+  const curlBtnHandler = () => {
+    setCopiedValue(
+      `curl "https://lit-chamber-70662.herokuapp.com/v1/review?apiKey=6e97db3d-269c-4309-ad0a-72ffb83cc0d5" `
+    );
+    setBtnActive({
+      browser: false,
+      curl: true,
+      python: false,
+      node: false,
+      php: false,
+    });
+  };
+
+  const pythonBtnHandler = () => {
+    setCopiedValue(
+      `curl "https://app.reviewapi.io/api/v1/batches/kzPdyP7bQr?apikey=YOUR-APIKEY" `
+    );
+    setBtnActive({
+      browser: false,
+      curl: false,
+      python: true,
+      node: false,
+      php: false,
+    });
+  };
+
+  const nodeBtnHandler = () => {
+    setCopiedValue(
+      `curl "https://app.reviewapi.io/api/v1/batches/kzPdyP7bQr?apikey=YOUR-APIKEY" `
+    );
+    setBtnActive({
+      browser: false,
+      curl: false,
+      python: false,
+      node: true,
+      php: false,
+    });
+  };
+
+  const phpBtnHandler = () => {
+    setCopiedValue(
+      `curl "https://app.reviewapi.io/api/v1/batches/kzPdyP7bQr?apikey=YOUR-APIKEY" `
+    );
+    setBtnActive({
+      browser: false,
+      curl: false,
+      python: false,
+      node: false,
+      php: true,
+    });
+  };
+
   return (
     <div className={classes.mainBox}>
       <div className={classes.prgmOption}>
         <div className={classes.options}>
-          <button>Browser</button>
+          <button className={classes[browserClass]} onClick={browserBtnHandler}>
+            BROWSER
+          </button>
+          <button className={classes[curlClass]} onClick={curlBtnHandler}>
+            CURL
+          </button>
+          <button className={classes[pythonClass]} onClick={pythonBtnHandler}>
+            PYTHON
+          </button>
+          <button className={classes[nodeClass]} onClick={nodeBtnHandler}>
+            NODE.JS
+          </button>
+          <button className={classes[phpClass]} onClick={phpBtnHandler}>
+            PHP
+          </button>
         </div>
         <div className={classes.copyBtn}>
           <CopyToClipboard text={copiedValue}>
@@ -30,7 +128,7 @@ const CodeShower = (e) => {
       <div className={classes.result}>
         <pre className={classes.preCode}>
           <div className={classes.browserCode} ref={browserCodeRef}>
-            https://lit-chamber-70662.herokuapp.com/v1/review?apiKey=YOUR_API_KEYhttps://lit-chamber-70662.herokuapp.com/v1/review?apiKey=6e97db3d-269c-4309-ad0a-72ffb83cc0d5
+            https://lit-chamber-70662.herokuapp.com/v1/review?apiKey=6e97db3d-269c-4309-ad0a-72ffb83cc0d5
           </div>
         </pre>
       </div>
